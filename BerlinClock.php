584e6b42486e05e68c2dc46db9e5cc0ce5fc9677
corentin-decoster <71266398+corentin-decoster@users.noreply.github.com>
2020-11-05 17:51:48 +0100

    method countSimpleMinute
    Axel D implementation countSimpleMinute

--- BerlinClock.php
+++ BerlinClock.php
@@ -5,6 +5,15 @@ class BerlinClock
 {
 
  function countSimpleMinute(string $str): int{
-    return 0;
+    if($str==="OOOO")
+        return 00;
+     if($str==="YOOO")
+         return 01;
+     if($str==="YYOO")
+         return 02;
+     if($str==="YYYO")
+         return 03;
+    if($str==="YYYY")
+        return 04;
  }
 }
--- BerlinClockTest.php
+++ BerlinClockTest.php
@@ -14,13 +14,14 @@ class BerlinClockTest extends TestCase
 
         $this->assertEquals(00,$actual,"for OOOO we have 0 minute");
     }
-    public function testSimpleMinuteRow(){
-        $this->assertEquals(00,00,"for OOOO we have 0 minute");
-        $this->assertEquals(04,04,"for YYYY we have 04 minute");
-        $this->assertEquals(34,34,"for OOOO we have 34 minute");
-        $this->assertEquals(59,59,"for YYYY we have 59 minute");
-        $this->assertEquals(00,00,"for OOOO we have 35 minute");
 
+    public function test_simple_minute_shouldReturn04(){
+        $BerlinClock = new BerlinClock();
+
+        $actual = $BerlinClock->countSimpleMinute("YYYY");
+
+        $this->assertEquals(04,$actual,"for YYYY we have 0 minute");
     }
+
 
 }
